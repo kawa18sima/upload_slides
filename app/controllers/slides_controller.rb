34730612557a442d27bf61e0d46e019efd572c27
Masaya Nakashima <kawa18sima@gmail.com>
2017-06-10 15:45:39 +0900

    Edit slides_controller.rb
    Processing slide list display
    Added slide upload processing

---
+++ app/controllers/slides_controller.rb
@@ -0,0 +1,30 @@
+class SlidesController < ApplicationController
+  def index
+    @slides = []
+    slide_path = Dir.glob(Rails.root.join('public', '*.pptx'))
+    slide_path.each do |path|
+      content = {}
+      content[:filepath] = '/'+File.basename(path)
+      content[:filename] = File.basename(path)
+      @slides.push(content)
+    end
+  end
+
+  def new
+  end
+
+  def create
+    slide = slide_params[:file]
+    output_path = Rails.root.join('public', slide.original_filename)
+    File.open(output_path, 'w+b') do |fp|
+      fp.write  slide.read
+    end
+    redirect_to action: :index
+  end
+  private
+
+  def slide_params
+    params.permit(:file)
+  end
+
+end
